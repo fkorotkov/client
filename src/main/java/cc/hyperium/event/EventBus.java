1999494efcc032c2d0909a3ab8215c2bf3966005
Reece Dunham <me@rdil.rocks>
2019-03-26 13:16:58 -0400

    cleanup eventbus

--- src/main/java/cc/hyperium/event/EventBus.java
+++ src/main/java/cc/hyperium/event/EventBus.java
@@ -16,10 +16,8 @@
  */
 
 package cc.hyperium.event;
-
 import com.google.common.reflect.TypeToken;
 import net.minecraft.client.Minecraft;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Comparator;
@@ -32,7 +30,6 @@ public class EventBus {
     public static final EventBus INSTANCE = new EventBus();
     public static boolean ALLOW_PROFILE = false;
     private HashMap<Class<?>, CopyOnWriteArrayList<EventSubscriber>> subscriptions = new HashMap<>();
-
     /**
      * Registers all methods of a class into the event system with
      * the {@link package me.kbrewster.blazeapi.api.event.InvokeEvent} annotation
@@ -42,7 +39,6 @@ public class EventBus {
     public void register(Object obj) {
         // also contains the class itself
         TypeToken<?> token = TypeToken.of(obj.getClass());
-
         Set superClasses = token.getTypes().rawTypes();
 
         // we also want to loop over the super classes, since declaredMethods only gets method in the class itself
@@ -56,13 +52,11 @@ public class EventBus {
                 if (method.getAnnotation(InvokeEvent.class) == null) {
                     continue;
                 }
-
                 if (method.getParameters()[0] == null) {
                     throw new IllegalArgumentException("Couldn't find parameter inside of " + method.getName() + "!");
                 }
 
                 Class<?> event = method.getParameters()[0].getType();
-
                 Priority priority = method.getAnnotation(InvokeEvent.class).priority();
                 method.setAccessible(true);
 
@@ -103,15 +97,6 @@ public class EventBus {
         this.subscriptions.values().forEach(map -> map.removeIf(it -> it.getInstance().getClass() == clazz));
     }
 
-
-    /**
-     * Invokes all of the methods which are inside of the classes
-     * registered to the event
-     *
-     * @param event Event that is being posted
-     */
-
-
     /**
      * Invokes all of the methods which are inside of the classes
      * registered to the event
@@ -133,7 +118,6 @@ public class EventBus {
 
             DOING SO WILL CAUSE THE PROFILER CLASS TO BE LOADED BEFORE OPTIFINE PATCHES THE CLASS.
             THIS WILL CAUSE THE ARM IN WALL BUG AND BREAKING OF FAST RENDER.
-
          */
         boolean profile = Minecraft.getMinecraft().isCallingFromMinecraftThread() && Minecraft.getMinecraft().theWorld != null && ALLOW_PROFILE;
         if (profile) {
@@ -155,7 +139,8 @@ public class EventBus {
                 Minecraft.getMinecraft().mcProfiler.endSection();
             }
         });
-        if (profile)
+        if (profile) {
             Minecraft.getMinecraft().mcProfiler.endSection();
+        }
     }
 }
