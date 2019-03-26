f3170e40ce12453b699b6ef16e5145e5ab7adab3
Reece Dunham <me@rdil.rocks>
2019-03-26 15:02:38 -0400

    Cleanup

--- src/main/java/me/semx11/autotip/gson/adapter/TypeAdapter.java
+++ src/main/java/me/semx11/autotip/gson/adapter/TypeAdapter.java
@@ -1,5 +1,4 @@
 package me.semx11.autotip.gson.adapter;
-
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -9,20 +8,14 @@ import com.google.gson.JsonSerializer;
 import java.lang.reflect.Type;
 
 public interface TypeAdapter<T> extends JsonSerializer<T>, JsonDeserializer<T> {
-
     @Override
-    default T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-            throws JsonParseException {
+    default T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         return this.deserialize(json);
     }
-
     @Override
     default JsonElement serialize(T src, Type typeOfSrc, JsonSerializationContext context) {
         return this.serialize(src);
     }
-
     T deserialize(JsonElement json);
-
     JsonElement serialize(T src);
-
 }
