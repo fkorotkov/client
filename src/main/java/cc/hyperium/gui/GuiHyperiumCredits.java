3ed7a34027fc3b1ec356b9fac53a78523f5ff0bb
Reece Dunham <me@rdil.rocks>
2019-03-26 22:28:35 -0400

    remove outdated and broken stuff

--- src/main/java/cc/hyperium/gui/GuiHyperiumCredits.java
+++ src/main/java/cc/hyperium/gui/GuiHyperiumCredits.java
@@ -1,11 +1,5 @@
 package cc.hyperium.gui;
-
 import cc.hyperium.Hyperium;
-import cc.hyperium.mods.sk1ercommon.Multithreading;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
@@ -14,33 +8,16 @@ import net.minecraft.client.renderer.texture.DynamicTexture;
 import org.apache.commons.io.IOUtils;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.StreamSupport;
 
 public class GuiHyperiumCredits extends HyperiumGui {
-
-    private static LinkedHashMap<String, DynamicTexture> contributors = new LinkedHashMap<>();
-    private static HashMap<String, Integer> commits = new HashMap<>();
-    private static String err = "";
-
-    static {
-        System.setProperty("http.agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
-        Multithreading.runAsync(() -> fetch(1));
-    }
-
     private GuiScreen prevGui;
     private int offY = 0;
-
     public GuiHyperiumCredits(GuiScreen prevGui) {
         this.prevGui = prevGui;
     }
@@ -50,18 +27,10 @@ public class GuiHyperiumCredits extends HyperiumGui {
         drawDefaultBackground();
         FontRenderer fr = Minecraft.getMinecraft().fontRendererObj;
         GlStateManager.scale(2f, 2f, 2f);
-        drawCenteredString(fr, err.isEmpty() ? (contributors.isEmpty() ? "Please wait" : "Jailbreak Contributors") : "Error: " + err, width / 4, 20 + offY / 2, 0xFFFFFF);
+        drawCenteredString(fr, "Maintained by jumbo", width / 4, 20 + offY / 2, 0xFFFFFF);
         GlStateManager.scale(0.5f, 0.5f, 0.5f);
         int x = width / 2 - 100;
         AtomicInteger y = new AtomicInteger(70 + offY);
-        contributors.forEach((c, i) -> {
-            GlStateManager.color(1f, 1f, 1f);
-            GlStateManager.bindTexture(i.getGlTextureId());
-            drawScaledCustomSizeModalRect(x, y.get(), 0, 0, 20, 20, 20, 20, 20, 20);
-            drawString(fr, c, x + 25, y.get(), 0xE0E0E0);
-            drawString(fr, commits.get(c) + " commits", x + 25, y.get() + 10, 0x757575);
-            y.addAndGet(25);
-        });
     }
 
     @Override
@@ -82,39 +51,9 @@ public class GuiHyperiumCredits extends HyperiumGui {
 
     @Override
     protected void keyTyped(char typedChar, int keyCode) throws IOException {
-        if (keyCode == Keyboard.KEY_ESCAPE)
+        if (keyCode == Keyboard.KEY_ESCAPE) {
             Hyperium.INSTANCE.getHandlers().getGuiDisplayHandler().setDisplayNextTick(prevGui);
+        }
         super.keyTyped(typedChar, keyCode);
     }
-
-    private static void fetch(int tries) {
-        try {
-            String content = IOUtils.toString(URI.create("https://api.github.com/repos/RDIL/Hyperium-Jailbreak/stats/contributors"), Charset.forName("UTF-8"));
-            JsonParser parser = new JsonParser();
-            JsonArray a = parser.parse(content).getAsJsonArray();
-            StreamSupport.stream(a.spliterator(), false).map(JsonElement::getAsJsonObject).filter(o -> o.has("total")).sorted(Comparator.comparingLong(o -> ((JsonObject) o).get("total").getAsInt()).reversed()).forEach(o -> {
-                JsonObject con = o.get("author").getAsJsonObject();
-                try {
-                    BufferedImage bi = ImageIO.read(new URL(con.get("avatar_url").getAsString() + "&size=20"));
-                    Minecraft.getMinecraft().addScheduledTask(() -> {
-                        DynamicTexture tx = new DynamicTexture(bi);
-                        try {
-                            tx.loadTexture(Minecraft.getMinecraft().getResourceManager());
-                            contributors.put(con.get("login").getAsString(), tx);
-                            commits.put(con.get("login").getAsString(), o.get("total").getAsInt());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }).get();
-                } catch (IOException | InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                }
-            });
-        } catch (Exception e) {
-            if (tries < 5) {
-                fetch(tries + 1);
-            }
-        }
-    }
-
 }
