3a5c69a201daa11756fbb408838f3bafab04b559
Reece Dunham <me@rdil.rocks>
2019-03-27 10:49:57 -0400

    cleanup

--- src/main/java/me/semx11/autotip/api/request/Request.java
+++ src/main/java/me/semx11/autotip/api/request/Request.java
@@ -1,12 +1,7 @@
 package me.semx11.autotip.api.request;
-
 import me.semx11.autotip.api.RequestType;
 import me.semx11.autotip.api.reply.Reply;
-
 public interface Request<T extends Reply> {
-
     T execute();
-
     RequestType getType();
-
 }
