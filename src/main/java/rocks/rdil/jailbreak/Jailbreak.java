077db5f4b70b25669c5211c7b326dba5d352236a
Reece Dunham <me@rdil.rocks>
2019-03-29 11:55:42 -0400

    remove stuff that is broken

--- src/main/java/rocks/rdil/jailbreak/Jailbreak.java
+++ src/main/java/rocks/rdil/jailbreak/Jailbreak.java
@@ -4,13 +4,12 @@ import cc.hyperium.Hyperium;
 
 public class Jailbreak {
     public Jailbreak() {}
-
     public void debug() {
-        Hyperium.LOGGER.debug("\n\n----------------");
+        Hyperium.LOGGER.debug("----------------");
         Hyperium.LOGGER.warn("This project is NOT RUN BY THE HYPERIUM TEAM");
         Hyperium.LOGGER.warn("Please report bugs by DMing jumbocakeyumyum#0001 on Discord");
         Hyperium.LOGGER.warn("or by emailing me@rdil.rocks");
         Hyperium.LOGGER.warn("Thanks!");
-        Hyperium.LOGGER.debug("----------------\n\n");
+        Hyperium.LOGGER.debug("----------------");
     }
 }
