1bc0ef7721db74145658a80431c1283d201c8752
Reece Dunham <me@rdil.rocks>
2019-04-01 12:36:21 -0400

    what is this spacing

--- src/main/kotlin/cc/hyperium/internal/addons/translate/MixinTranslator.kt
+++ src/main/kotlin/cc/hyperium/internal/addons/translate/MixinTranslator.kt
@@ -4,9 +4,7 @@ import cc.hyperium.internal.addons.AddonManifest
 import org.spongepowered.asm.mixin.Mixins
 
 class MixinTranslator : AbstractTranslator() {
-
     override fun translate(manifest: AddonManifest) {
         manifest.mixinConfigs?.forEach(Mixins::addConfiguration)
     }
-
 }
