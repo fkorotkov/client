cfdf783878455d0c443d1aa83423192c2ed6bb55
Reece Dunham <me@rdil.rocks>
2019-04-01 08:44:18 -0400

    cleanup

--- src/main/java/cc/hyperium/addons/InternalAddons.java
+++ src/main/java/cc/hyperium/addons/InternalAddons.java
@@ -1,5 +1,5 @@
 package cc.hyperium.addons;
-
+import cc.hyperium.config.Settings;
 import cc.hyperium.addons.bossbar.BossbarAddon;
 import cc.hyperium.addons.customcrosshair.CustomCrosshairAddon;
 import cc.hyperium.addons.sidebar.SidebarAddon;
@@ -11,9 +11,14 @@ public class InternalAddons {
     private final BossbarAddon bossbarAddon;
 
     public InternalAddons() {
-        this.customCrosshairAddon = ((CustomCrosshairAddon) new CustomCrosshairAddon().init());
-        this.sidebarAddon = ((SidebarAddon) new SidebarAddon().init());
-        this.bossbarAddon = ((BossbarAddon) new BossbarAddon().init());
+        this.customCrosshairAddon = (CustomCrosshairAddon) new CustomCrosshairAddon();
+        this.sidebarAddon = (SidebarAddon) new SidebarAddon();
+        this.bossbarAddon = (BossbarAddon) new BossbarAddon();
+        if (!Settings.FPS) {
+            this.customCrosshairAddon.init();
+        }
+        this.sidebarAddon.init();
+        this.bossbarAddon.init();
     }
 
     public CustomCrosshairAddon getCustomCrosshairAddon() {
