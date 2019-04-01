8ae8efb5d074b322ea4dd33d78ba24a4a90195dc
Reece Dunham <me@rdil.rocks>
2019-04-01 12:41:04 -0400

    cleanup

--- src/main/kotlin/cc/hyperium/internal/addons/strategy/DefaultAddonLoader.kt
+++ src/main/kotlin/cc/hyperium/internal/addons/strategy/DefaultAddonLoader.kt
@@ -6,7 +6,6 @@ import cc.hyperium.internal.addons.AddonManifest
 import cc.hyperium.internal.addons.misc.AddonLoadException
 import cc.hyperium.internal.addons.misc.AddonManifestParser
 import net.minecraft.launchwrapper.Launch
-
 import java.io.File
 import java.util.jar.JarFile
 
@@ -29,7 +28,6 @@ class DefaultAddonLoader : AddonLoaderStrategy() {
         if (file == null) {
             throw AddonLoadException("Could not load file; parameter issued was null.")
         }
-
         val jar = JarFile(file)
         if (jar.getJarEntry("pack.mcmeta") != null) {
             AddonBootstrap.addonResourcePacks.add(file)
@@ -41,7 +39,6 @@ class DefaultAddonLoader : AddonLoaderStrategy() {
         }
         val uri = file.toURI()
         Launch.classLoader.addURL(uri.toURL())
-
         return manifest
     }
 }
