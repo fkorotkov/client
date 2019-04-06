43fbf0caeb6bcefecc6796e70b6ab9215ddd42c0
Reece Dunham <me@rdil.rocks>
2019-04-06 12:20:25 -0400

    Provided libs update

--- src/main/resources/providedLibs.js
+++ src/main/resources/providedLibs.js
@@ -2,33 +2,24 @@
    Java
 */
 
-// Extra libs
 var ArrayList = Java.type("java.util.ArrayList");
 var HashMap = Java.type("java.util.HashMap");
 var Thread = Java.type("java.lang.Thread");
 var Keyboard = Java.type("org.lwjgl.input.Keyboard");
-
-// Triggers
 var TriggerRegister = Java.type("com.chattriggers.ctjs.engine.langs.js.JSRegister").INSTANCE;
 var TriggerResult = Java.type("com.chattriggers.ctjs.triggers.OnTrigger.TriggerResult");
 var Priority = Java.type("com.chattriggers.ctjs.triggers.OnTrigger.Priority");
-
-// Libraries
 var ChatLib = Java.type("com.chattriggers.ctjs.minecraft.libs.ChatLib");
 var EventLib = Java.type("com.chattriggers.ctjs.minecraft.libs.EventLib");
-
 var Renderer = Java.type("com.chattriggers.ctjs.minecraft.libs.renderer.Renderer");
 var Shape = Java.type("com.chattriggers.ctjs.minecraft.libs.renderer.Shape");
 var Rectangle = Java.type("com.chattriggers.ctjs.minecraft.libs.renderer.Rectangle");
 var Text = Java.type("com.chattriggers.ctjs.minecraft.libs.renderer.Text");
 var Image = Java.type("com.chattriggers.ctjs.minecraft.libs.renderer.Image");
-
 var Tessellator = Java.type("com.chattriggers.ctjs.minecraft.libs.Tessellator").INSTANCE;
 var FileLib = Java.type("com.chattriggers.ctjs.minecraft.libs.FileLib");
 var MathLib = Java.type("com.chattriggers.ctjs.minecraft.libs.MathLib");
 var XMLHttpRequest = Java.type("com.chattriggers.ctjs.engine.langs.js.JSXMLHttpRequest");
-
-// Objects
 var Display = Java.type("com.chattriggers.ctjs.engine.langs.js.JSDisplay");
 var DisplayLine = Java.type("com.chattriggers.ctjs.engine.langs.js.JSDisplayLine");
 var DisplayHandler = Java.type("com.chattriggers.ctjs.minecraft.objects.display.DisplayHandler");
@@ -39,8 +30,6 @@ var Book = Java.type("com.chattriggers.ctjs.minecraft.objects.Book");
 var KeyBind = Java.type("com.chattriggers.ctjs.minecraft.objects.KeyBind");
 var Image = Java.type("com.chattriggers.ctjs.minecraft.libs.renderer.Image");
 var Sound = Java.type("com.chattriggers.ctjs.minecraft.objects.Sound");
-
-// Wrappers
 var Client = Java.type("com.chattriggers.ctjs.minecraft.wrappers.Client");
 var Player = Java.type("com.chattriggers.ctjs.minecraft.wrappers.Player");
 var World = Java.type("com.chattriggers.ctjs.minecraft.wrappers.World");
@@ -57,8 +46,6 @@ var Action = Java.type("com.chattriggers.ctjs.minecraft.wrappers.objects.invento
 var ClickAction = Java.type("com.chattriggers.ctjs.minecraft.wrappers.objects.inventory.action.ClickAction");
 var DragAction = Java.type("com.chattriggers.ctjs.minecraft.wrappers.objects.inventory.action.DragAction");
 var KeyAction = Java.type("com.chattriggers.ctjs.minecraft.wrappers.objects.inventory.action.KeyAction");
-
-// Triggers
 var OnChatTrigger = Java.type("com.chattriggers.ctjs.triggers.OnChatTrigger");
 var OnCommandTrigger = Java.type("com.chattriggers.ctjs.triggers.OnCommandTrigger");
 var OnRegularTrigger = Java.type("com.chattriggers.ctjs.triggers.OnRegularTrigger");
@@ -66,13 +53,9 @@ var OnRenderTrigger = Java.type("com.chattriggers.ctjs.triggers.OnRenderTrigger"
 var OnSoundPlayTrigger = Java.type("com.chattriggers.ctjs.triggers.OnSoundPlayTrigger");
 var OnStepTrigger = Java.type("com.chattriggers.ctjs.triggers.OnStepTrigger");
 var OnTrigger = Java.type("com.chattriggers.ctjs.triggers.OnTrigger");
-
-// Misc
 var Console = Java.type("com.chattriggers.ctjs.engine.langs.js.JSLoader").INSTANCE.getConsole();
 var Config = Java.type("com.chattriggers.ctjs.utils.config.Config").INSTANCE;
 var ChatTriggers = Java.type("com.chattriggers.ctjs.Reference").INSTANCE;
-
-/*End Built in Vars */
 
 
 // simplified methods
