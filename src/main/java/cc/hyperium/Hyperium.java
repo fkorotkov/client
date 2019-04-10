a8b2af93ff24c135d6cfeb000a042c997e688ac6
Reece Dunham <me@rdil.rocks>
2019-04-10 09:30:33 -0400

    Fix

--- src/main/java/cc/hyperium/Hyperium.java
+++ src/main/java/cc/hyperium/Hyperium.java
@@ -62,7 +62,7 @@ import net.minecraft.crash.CrashReport;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.Display;
-import org.cef.OS;
+import rocks.rdil.jailbreak.util.OS;
 import rocks.rdil.jailbreak.plugin.ThankWatchdog;
 import rocks.rdil.jailbreak.Jailbreak;
 import java.io.BufferedReader;
