c8ea4982e9f69c421a5a1f8cbe670e19620cfe24
Reece Dunham <me@rdil.rocks>
2019-04-10 18:11:13 -0400

    cleanup

--- src/main/java/cc/hyperium/utils/LaunchUtil.java
+++ src/main/java/cc/hyperium/utils/LaunchUtil.java
@@ -1,33 +1,25 @@
 package cc.hyperium.utils;
 
 import cc.hyperium.Hyperium;
-
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
-
 import net.minecraft.client.Minecraft;
+import rocks.rdil.jailbreak.util.OS;
 
 /**
  * @author KodingKing
  */
 public class LaunchUtil {
-
     public static void launch() {
         try {
-            boolean windows = InstallerUtils.getOS() == InstallerUtils.OSType.Windows;
-            //Class<?> c = getClass();
-            //String n = c.getName().replace('.', '/');
-            String cs = "";
+            String cs;
             for (URL u : ((URLClassLoader) Hyperium.class.getClassLoader()).getURLs()) {
-                if (u.getPath().contains("Hyperium")) {
-                    cs = u.getPath();
-                }
+                if (u.getPath().contains("Hyperium")) cs = u.getPath();
             }
-            System.out.println("cs=" + cs);
             Runtime.getRuntime().exec(new String[]{
-                windows ? "cmd" : "bash",
-                windows ? "/c" : "-c",
+                OS.isWindows() ? "cmd" : "bash",
+                OS.isWindows() ? "/c" : "-c",
                 "java",
                 "-jar",
                 cs,
@@ -37,5 +29,4 @@ public class LaunchUtil {
             e.printStackTrace();
         }
     }
-
 }
