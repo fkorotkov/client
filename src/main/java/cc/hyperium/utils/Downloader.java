30ff2e689ab618921101f25a4a4fa6dff073a367
Reece Dunham <me@rdil.rocks>
2019-04-10 17:36:27 -0400

    cleanup

--- src/main/java/cc/hyperium/utils/Downloader.java
+++ src/main/java/cc/hyperium/utils/Downloader.java
@@ -9,7 +9,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,9 +17,7 @@ import java.net.URL;
 /**
  * @author ConorTheDev
  */
-
 public class Downloader {
-
     public File download(URL url, File dstFile) {
         CloseableHttpClient httpclient = HttpClients.custom()
             .setRedirectStrategy(new LaxRedirectStrategy()) // adds HTTP REDIRECT support to GET and POST methods
@@ -34,22 +31,16 @@ public class Downloader {
             IOUtils.closeQuietly(httpclient);
         }
     }
-
     static class FileDownloadResponseHandler implements ResponseHandler<File> {
-
         private final File target;
-
         public FileDownloadResponseHandler(File target) {
             this.target = target;
         }
-
         @Override
         public File handleResponse(HttpResponse response) throws IOException {
             InputStream source = response.getEntity().getContent();
             FileUtils.copyInputStreamToFile(source, this.target);
             return this.target;
         }
-
     }
-
 }
