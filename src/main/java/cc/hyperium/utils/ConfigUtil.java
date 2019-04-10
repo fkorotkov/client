16c1b0d21ed306ffe9fc2dc63f6260334618002a
Reece Dunham <me@rdil.rocks>
2019-04-10 18:06:19 -0400

    cleanup

--- src/main/java/cc/hyperium/utils/ConfigUtil.java
+++ src/main/java/cc/hyperium/utils/ConfigUtil.java
@@ -7,47 +7,38 @@ public class ConfigUtil {
 
     public static boolean getOptionWithDefault(String settingName, String classPath, boolean defaultObject) {
         JsonObject generalJsonObject = Hyperium.CONFIG.getConfig().get(classPath).getAsJsonObject();
-        boolean value;
         if (!generalJsonObject.has(settingName)) {
-            System.out.println(classPath + " " + settingName + " " + defaultObject);
             generalJsonObject.addProperty(settingName, defaultObject);
             saveConfig();
         }
-        value = generalJsonObject.get(settingName).getAsBoolean();
-        return value;
+        return generalJsonObject.get(settingName).getAsBoolean();
     }
 
     public static double getOptionWithDefault(String settingName, String classPath, Number defaultObject) {
         JsonObject generalJsonObject = Hyperium.CONFIG.getConfig().get(classPath).getAsJsonObject();
-        double value;
         if (!generalJsonObject.has(settingName)) {
             generalJsonObject.addProperty(settingName, defaultObject);
             saveConfig();
         }
-        value = generalJsonObject.get(settingName).getAsDouble();
-        return value;
+        return generalJsonObject.get(settingName).getAsDouble();
     }
 
     public static String getOptionWithDefault(String settingName, String classPath, String defaultObject) {
         JsonObject generalJsonObject = Hyperium.CONFIG.getConfig().get(classPath).getAsJsonObject();
-        String value;
         if (!generalJsonObject.has(settingName)) {
             generalJsonObject.addProperty(settingName, defaultObject);
             saveConfig();
         }
-        value = generalJsonObject.get(settingName).getAsString();
-        return value;
+        return generalJsonObject.get(settingName).getAsString();
     }
 
     public static char getOptionWithDefault(String settingName, String classPath, char defaultObject) {
         JsonObject generalJsonObject = Hyperium.CONFIG.getConfig().get(classPath).getAsJsonObject();
-        char value;
         if (!generalJsonObject.has(settingName)) {
             generalJsonObject.addProperty(settingName, defaultObject);
             saveConfig();
         }
-        value = generalJsonObject.get(settingName).getAsCharacter();
-        return value;
+        return generalJsonObject.get(settingName).getAsCharacter();
     }
 
     public static void saveConfig() {
@@ -57,5 +48,4 @@ public class ConfigUtil {
     public static void register(Object obj) {
         Hyperium.CONFIG.register(obj);
     }
-
 }
