790f6b4531e7a38b5e168ec58e74b902908e0677
Reece Dunham <me@rdil.rocks>
2019-04-11 12:40:37 -0400

    cleanup

--- src/main/java/cc/hyperium/handlers/handlers/SettingsMigrator.java
+++ src/main/java/cc/hyperium/handlers/handlers/SettingsMigrator.java
@@ -4,23 +4,15 @@ import cc.hyperium.Hyperium;
 import cc.hyperium.config.DefaultConfig;
 import cc.hyperium.mods.chromahud.ChromaHUD;
 import cc.hyperium.mods.levelhead.Levelhead;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-/**
- * Created by mitchellkatz on 6/22/18. Designed for production use on Sk1er.club
- */
 public class SettingsMigrator {
-
-
     public void migrate() {
         File folder = Hyperium.folder;
         File config = new File(folder.getParentFile(), "config");
-        if (!config.exists())
-            return;
-
+        if (!config.exists()) return;
         File chromahud = new File(config, "ChromaHUD.cfg");
         if (chromahud.exists()) {
             try {
@@ -30,13 +22,10 @@ public class SettingsMigrator {
             }
             Hyperium.INSTANCE.getModIntegration().getChromaHUD().setup();
         }
-
         File level_head = new File(config, "LEVEL_HEAD.cfg");
         if (level_head.exists()) {
             DefaultConfig defaultConfig = new DefaultConfig(level_head);
             defaultConfig.register(Hyperium.INSTANCE.getModIntegration().getLevelhead().getConfig());
         }
-
-
     }
 }
