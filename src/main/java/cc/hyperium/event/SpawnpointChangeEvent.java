2e550303e1ef7fedc07ad4f36a9dd10274de04f0
Reece Dunham <me@rdil.rocks>
2019-04-11 12:46:08 -0400

    remove javadoc

--- src/main/java/cc/hyperium/event/SpawnpointChangeEvent.java
+++ src/main/java/cc/hyperium/event/SpawnpointChangeEvent.java
@@ -1,13 +1,7 @@
 package cc.hyperium.event;
-
 import net.minecraft.util.BlockPos;
 
-/**
- * Invoked when the spawnpoint has changed
- * This is useful for detecting minigames
- */
 public class SpawnpointChangeEvent extends Event {
-
     private final BlockPos blockPos;
 
     public SpawnpointChangeEvent(BlockPos blockPos) {
