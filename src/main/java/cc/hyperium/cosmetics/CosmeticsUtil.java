8f9cc05134131d34dcece4c07c911479218e24f7
Reece Dunham <me@rdil.rocks>
2019-04-11 13:01:51 -0400

    cleanup

--- src/main/java/cc/hyperium/cosmetics/CosmeticsUtil.java
+++ src/main/java/cc/hyperium/cosmetics/CosmeticsUtil.java
@@ -22,11 +22,8 @@ import cc.hyperium.config.Settings;
 import cc.hyperium.purchases.EnumPurchaseType;
 
 public class CosmeticsUtil {
-
     public static boolean shouldHide(EnumPurchaseType type) {
-        if (Settings.SHOW_COSMETICS_EVERYWHERE) {
-            return false;
-        }
+        if (Settings.SHOW_COSMETICS_EVERYWHERE) return false;
         return !Hyperium.INSTANCE.getHandlers().getLocationHandler().isLobbyOrHousing();
     }
 }
