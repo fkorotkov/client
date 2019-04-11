daa48e8c5f48d30498af30d54042372ba167b91e
Reece Dunham <me@rdil.rocks>
2019-04-11 12:43:42 -0400

    cleanup

--- src/main/java/cc/hyperium/handlers/handlers/OtherConfigOptions.java
+++ src/main/java/cc/hyperium/handlers/handlers/OtherConfigOptions.java
@@ -1,12 +1,7 @@
 package cc.hyperium.handlers.handlers;
 
-/**
- * @author Sk1er
- */
 public class OtherConfigOptions {
-
     public int renderNameDistance = 64;
     public int stringCacheSize = 1000;
     public boolean isCancelBox;
-
 }
