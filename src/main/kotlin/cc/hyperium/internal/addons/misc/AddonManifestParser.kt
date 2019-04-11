38bf9968fc821723e245e5e7254ff1ac1431937d
Reece Dunham <me@rdil.rocks>
2019-04-11 10:53:53 -0400

    cleanup + fix jitpack.io

--- src/main/kotlin/cc/hyperium/internal/addons/misc/AddonManifestParser.kt
+++ src/main/kotlin/cc/hyperium/internal/addons/misc/AddonManifestParser.kt
@@ -10,70 +10,40 @@ import java.io.*
 import java.nio.charset.Charset
 import java.util.jar.JarFile
 
-/**
- * Parses addon.json into a instance of {@link cc.hyperium.internal.addons.AddonManifest}
- *
- * @since 1.0
- * @author Kevin Brewster
- */
 class AddonManifestParser {
     private var json: JsonObject? = null
     private val gson = Gson()
 
-    /**
-     * Used when parsing the addon.json inside of a
-     * jarfile
-     *
-     * @param jar jarfile which the manifest should be read from
-     */
     constructor(jar: JarFile) {
 
         var jarInputStream: InputStream? = null
         try {
             val entry = jar.getEntry("addon.json")
-
             val jsonFile = File.createTempFile("json", "tmp")
             jsonFile.deleteOnExit()
-
             jarInputStream = jar.getInputStream(entry)
             val os = FileOutputStream(jsonFile)
             copyInputStream(jarInputStream, os)
-
             val contents = Files.toString(jsonFile, Charset.defaultCharset())
-
             val parser = JsonParser()
             val json = parser.parse(contents).asJsonObject
-
-            if (!json.has("version") && !json.has("name") && !json.has("mainClass")) {
-                throw AddonLoadException("Invalid addon manifest ( Must include name, verson and mainClass)")
-            }
+            if (!json.has("version") && !json.has("name") && !json.has("mainClass")) throw AddonLoadException("Invalid addon manifest ( Must include name, verson and mainClass)")
             this.json = json
         } catch (e: Exception) {
             e.printStackTrace()
             throw AddonLoadException("Exception reading manifest")
         } finally {
-            if (jarInputStream != null) {
-                jarInputStream.close()
-            }
+            if (jarInputStream != null) jarInputStream.close()
             jar.close()
         }
     }
 
-    /**
-     * Used when parsing an addon.jsons string
-     *
-     * @param contents The addon.json
-     */
     constructor(contents: String) {
         val parser = JsonParser()
         val json = parser.parse(contents).asJsonObject
 
-        if (!json.has("version") && !json.has("name") && !json.has("mainClass")) {
-            throw AddonLoadException("Invalid addon manifest (Must include name, version and mainClass)")
-        }
-        if (json.has("overlay")) {
-            OverlayChecker.checkOverlayField(json.get("overlay").asString)
-        }
+        if (!json.has("version") && !json.has("name") && !json.has("mainClass")) throw AddonLoadException("Invalid addon manifest (Must include name, version and mainClass)")
+        if (json.has("overlay")) OverlayChecker.checkOverlayField(json.get("overlay").asString)
         this.json = json
     }
 
@@ -81,11 +51,6 @@ class AddonManifestParser {
         return gson.fromJson(json, AddonManifest::class.java)
     }
 
-    /**
-     * The String of the json
-     *
-     * @return addon manifest json in string
-     */
     override fun toString(): String {
         return json!!.toString()
     }
