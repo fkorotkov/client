6bc093cce93a92c4f93bb5f483ec925b108e314f
Reece Dunham <me@rdil.rocks>
2019-04-11 10:54:51 -0400

    cleanup

--- src/main/kotlin/cc/hyperium/internal/addons/translate/InstanceTranslator.kt
+++ src/main/kotlin/cc/hyperium/internal/addons/translate/InstanceTranslator.kt
@@ -1,20 +1,6 @@
 package cc.hyperium.internal.addons.translate
-
 import cc.hyperium.internal.addons.AddonManifest
 
 class InstanceTranslator : AbstractTranslator() {
-
-    override fun translate(manifest: AddonManifest) {
-        /*  val instance = Class.forName(manifest.mainClass).newInstance()
-          val clazz = instance.javaClass
-          val fields = clazz.fields
-          for (field in fields) {
-              val annotation = field.getAnnotation(Instance::class.java)
-              if (annotation != null) {
-                  field.isAccessible = true
-                  field.set(instance, instance)
-              }
-          }*/
-    }
-
+    override fun translate(manifest: AddonManifest) {}
 }
