de934b52a8cfe2d6c19aa580702186157b458c5d
Reece Dunham <me@rdil.rocks>
2019-04-11 11:33:03 -0400

    remove javadocs

--- src/main/java/cc/hyperium/event/HypixelWinEvent.java
+++ src/main/java/cc/hyperium/event/HypixelWinEvent.java
@@ -18,21 +18,15 @@
 package cc.hyperium.event;
 
 import java.util.List;
-
 import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * Invoked when player(s) win a game
- */
 public final class HypixelWinEvent extends Event {
-
     @NotNull
-    private final List winners;
+    private final List winners
 
     public HypixelWinEvent(@NotNull List winners) {
         Preconditions.checkNotNull(winners, "winners");
-
         this.winners = winners;
     }
 
