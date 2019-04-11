bfc3df04a9ddee0d25ce8a12ab3d410b1cd86bc7
Reece Dunham <me@rdil.rocks>
2019-04-11 18:39:19 -0400

    Remove javadocs

--- src/main/java/cc/hyperium/event/RenderHUDEvent.java
+++ src/main/java/cc/hyperium/event/RenderHUDEvent.java
@@ -1,10 +1,6 @@
 package cc.hyperium.event;
 
-/**
- * Invoked when the hud of the client is rendered
- */
 public class RenderHUDEvent extends Event {
-
     private final float partialTicks;
 
     public RenderHUDEvent(float partialTicks) {
