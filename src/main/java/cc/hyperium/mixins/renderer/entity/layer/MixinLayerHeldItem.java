a0e09b980686607d6bc0b65b4032a7e96129fcdf
--replace-all <--replace-all>
2019-04-11 20:43:41 -0400

    remove old javadocs

--- src/main/java/cc/hyperium/mixins/renderer/entity/layer/MixinLayerHeldItem.java
+++ src/main/java/cc/hyperium/mixins/renderer/entity/layer/MixinLayerHeldItem.java
@@ -11,17 +11,12 @@ import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(LayerHeldItem.class)
 public class MixinLayerHeldItem {
-
     private OldBlocking oldBlocking = new OldBlocking();
 
     @Shadow
     @Final
     private RendererLivingEntity<?> livingEntityRenderer;
 
-    /**
-     * @author SiroQ
-     * @reason Adds compatibility for the 1.7 blocking animation
-     */
     @Overwrite
     public void doRenderLayer(EntityLivingBase entity, float f, float f2, float f3, float partialTicks, float f4, float f5, float scale) {
         oldBlocking.doRenderLayer(entity, this.livingEntityRenderer);
