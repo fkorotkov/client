c3f7e5db89e70f47c8b59ebadb7fea966d37baa7
Reece Dunham <me@rdil.rocks>
2019-04-12 08:49:55 -0400

    cleanup

--- src/main/java/cc/hyperium/handlers/handlers/chat/FriendRequestChatHandler.java
+++ src/main/java/cc/hyperium/handlers/handlers/chat/FriendRequestChatHandler.java
@@ -4,12 +4,10 @@ import cc.hyperium.event.EventBus;
 import cc.hyperium.event.FriendRemoveEvent;
 import cc.hyperium.event.HypixelFriendRequestEvent;
 import net.minecraft.util.IChatComponent;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FriendRequestChatHandler extends HyperiumChatHandler {
-
     @Override
     public boolean chatReceived(IChatComponent component, String text) {
         Matcher matcher1 = Pattern.compile("You removed ((?<rank>\\[.+] )?(?<player>\\w+)) from your friends list!").matcher(text);
@@ -17,22 +15,13 @@ public class FriendRequestChatHandler extends HyperiumChatHandler {
             String rank = "";
             try {
                 rank = matcher1.group("rank");
-            } catch (Exception ignored) {
-
-            }
+            } catch (Exception ignored) {}
             String player = matcher1.group("player");
             EventBus.INSTANCE.post(new FriendRemoveEvent(rank + player, player));
         }
-        if (!text.toLowerCase().contains("friend request")) {
-            return false;
-        }
-
+        if (!text.toLowerCase().contains("friend request")) return false;
         Matcher matcher = regexPatterns.get(ChatRegexType.FRIEND_REQUEST).matcher(text);
-        if (matcher.find()) {
-            EventBus.INSTANCE.post(new HypixelFriendRequestEvent(matcher.group("player")));
-        }
-
-
+        if (matcher.find()) EventBus.INSTANCE.post(new HypixelFriendRequestEvent(matcher.group("player")));
         return false;
     }
 }
