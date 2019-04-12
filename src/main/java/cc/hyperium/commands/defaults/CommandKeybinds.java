6a622b542c43023a3bfc8bfcd2fc4d089049b41f
Reece Dunham <me@rdil.rocks>
2019-04-12 08:58:56 -0400

    cleanup

--- src/main/java/cc/hyperium/commands/defaults/CommandKeybinds.java
+++ src/main/java/cc/hyperium/commands/defaults/CommandKeybinds.java
@@ -19,27 +19,22 @@ package cc.hyperium.commands.defaults;
 
 import cc.hyperium.commands.BaseCommand;
 import cc.hyperium.gui.keybinds.GuiKeybinds;
-
 import java.util.Collections;
 import java.util.List;
 
 public class CommandKeybinds implements BaseCommand {
-
     @Override
     public String getName() {
         return "keybinds";
     }
-
     @Override
     public String getUsage() {
         return "/keybinds";
     }
-
     @Override
     public List<String> getCommandAliases() {
         return Collections.singletonList("controls");
     }
-
     @Override
     public void onExecute(String[] args) {
         new GuiKeybinds().show();
