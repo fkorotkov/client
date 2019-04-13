f49dcd10178cf014b385e15f8fbf872b9fc17a77
Reece Dunham <me@rdil.rocks>
2019-04-13 07:43:48 -0400

    Cleanup

--- src/main/java/cc/hyperium/commands/defaults/CommandClearChat.java
+++ src/main/java/cc/hyperium/commands/defaults/CommandClearChat.java
@@ -17,19 +17,10 @@
 
 package cc.hyperium.commands.defaults;
 
-
 import cc.hyperium.commands.BaseCommand;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiNewChat;
 
-/**
- * A simple command to clear your chat history & sent commands,
- * simply calls the {@link GuiNewChat#clearChatMessages()} method
- *
- * @author boomboompower
- */
 public class CommandClearChat implements BaseCommand {
-
     @Override
     public String getName() {
         return "clearchat";
