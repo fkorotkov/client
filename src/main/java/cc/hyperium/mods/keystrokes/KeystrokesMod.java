d5aa3cc5d08469d13130513abf7ca5b37f58910f
Reece Dunham <me@rdil.rocks>
2019-04-13 21:08:56 -0400

    cleanup + work to fix build

--- src/main/java/cc/hyperium/mods/keystrokes/KeystrokesMod.java
+++ src/main/java/cc/hyperium/mods/keystrokes/KeystrokesMod.java
@@ -26,42 +26,24 @@ import cc.hyperium.mods.sk1ercommon.Sk1erMod;
 import cc.hyperium.utils.ChatColor;
 
 public class KeystrokesMod extends AbstractMod {
-
-    /**
-     * The mods metadata
-     */
     private final Metadata metaData;
 
     private KeystrokesSettings config;
     private KeystrokesRenderer renderer;
 
-    /**
-     * Default constructor, this will load the mods metadata
-     */
     public KeystrokesMod() {
         Metadata data = new Metadata(this, "KeystrokesMod", "5.0.1", "Fyu, boomboompower and Sk1er");
-
         data.setDisplayName(ChatColor.AQUA + "Keystrokes");
-
         this.metaData = data;
     }
 
-    /**
-     * Init method, loads configs and events for the KeystrokesMod
-     *
-     * @return this mods instance
-     */
     @Override
     public AbstractMod init() {
         this.config = new KeystrokesSettings(this, Hyperium.folder);
         this.config.load();
-
-        new Sk1erMod("keystrokesmod", "5.0.1").checkStatus();
-
+        new Sk1erMod("keystrokesmod", "5.0.1");
         this.renderer = new KeystrokesRenderer(this);
-
         EventBus.INSTANCE.register(this.renderer);
-
         Hyperium.INSTANCE.getHandlers().getHyperiumCommandHandler().registerCommand(new CommandKeystrokes(this));
 
         return this;
@@ -72,20 +54,10 @@ public class KeystrokesMod extends AbstractMod {
         return this.metaData;
     }
 
-    /**
-     * Getter for the Keystrokes settings
-     *
-     * @return the keystrokes settings
-     */
     public KeystrokesSettings getSettings() {
         return this.config;
     }
 
-    /**
-     * Getter for the Keystrokes renderer
-     *
-     * @return the keystrokes renderer
-     */
     public KeystrokesRenderer getRenderer() {
         return this.renderer;
     }
